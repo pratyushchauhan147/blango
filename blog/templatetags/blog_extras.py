0f8ff38819225b32474eeeb3f47f374d13359671
PRATYUSH CHAUHAN <22051960@kiit.ac.in>
2025-06-11 18:34:16 +0000

    custom tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,7 +2,12 @@ from django.contrib.auth import get_user_model
 user_model = get_user_model()
 from django import template
 register = template.Library()
+from blog.models import Post
 
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
 
 @register.filter
 def author_details(author):
@@ -17,4 +22,36 @@ def author_details(author):
 
     return name
 
+@register.simple_tag
+def row():
+    return '<div class="row">'
+
+
+@register.simple_tag
+def endrow():
+    return "</div>"
+    
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
 
