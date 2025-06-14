f20269c5b92a4984e56bbab3cd00d2872dd2c0c4
22051960 <pratyushchauhan147code@gmail.com>
2025-06-14 22:04:34 +0530

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -14,21 +14,12 @@ urlpatterns = [
     path("", blog.views.index, name="home"),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
-
-    # Custom Registration URL FIRST to override django_registration default
     path("accounts/register/", RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register"),
-
-    # django-registration activation flow (activation emails, etc.)
     path("accounts/", include("django_registration.backends.activation.urls")),
-
-    # Django's built-in login/logout/password change URLs
     path("accounts/", include("django.contrib.auth.urls")),
-
-    # Django Allauth URLs (for social authentication like Google)
     path("accounts/", include("allauth.urls")),
-
-    # Profile page after login
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path("api/v1/", include("blog.api_urls")),
 ]
 
 if settings.DEBUG:
