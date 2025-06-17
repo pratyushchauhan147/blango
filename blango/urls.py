2893121a15ccf747e1a79402ab5ac5286acc7f61
PRATYUSH CHAUHAN <22051960@kiit.ac.in>
2025-06-17 19:07:33 +0000

    Finish JavaScript intro and fundamentals

--- blango/urls.py
+++ blango/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("", blog.views.index, name="home"),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
+        path("post-table/", blog.views.post_table, name="blog-post-table"),
     path("accounts/register/", RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register"),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
--- blog/views.py
+++ blog/views.py
@@ -42,3 +42,7 @@ def index(request):
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
+
+
+def post_table(request):
+    return render(request, "blog/post-table.html")  
